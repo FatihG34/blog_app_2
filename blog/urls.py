2c2206c923593e1b6278193054498b6bee7bf31a
FatihG34 <fthgnydn34@gmail.com>
2022-09-11 23:21:31 +0300

    post created

--- blog/urls.py
+++ blog/urls.py
@@ -1,8 +1,9 @@
 from django.urls import path
 
-from blog.views import post_list
+from blog.views import post_list, post_create
 
-
+app_name = 'blog'
 urlpatterns = [
-    path('', post_list, name='list')
+    path('', post_list, name='list'),
+    path('create/', post_create, name='create')
 ]
--- blog/views.py
+++ blog/views.py
@@ -1,5 +1,6 @@
-from django.shortcuts import render
-
+from urllib import request
+from django.shortcuts import render, redirect
+from .forms import PostForm
 from blog.models import Post
 
 # Create your views here.
@@ -9,3 +10,17 @@ def post_list(request):
         'object_list':qs
     }
     return render(request, 'blog/post_list.html', context)
+
+def post_create(request):
+    form = PostForm()
+    if request.method == 'POST':
+        form = PostForm(request.POST, request.FILES)
+        if form.is_valid():
+            post = form.save(commit=False)
+            post.author = request.user
+            post.save()
+            return redirect('blog:list')
+    context = {
+        'form':form
+    }
+    return render(request, 'blog/post_create.html',context)
