eccb92d80146926ff4de182235f9abf6b633bc82
FatihG34 <fthgnydn34@gmail.com>
2022-09-12 12:13:51 +0300

    blog post update done

--- blog/views.py
+++ blog/views.py
@@ -2,6 +2,7 @@ from urllib import request
 from django.shortcuts import render, redirect
 from .forms import PostForm
 from blog.models import Post
+from django.shortcuts import get_object_or_404
 
 # Create your views here.
 def post_list(request):
@@ -24,3 +25,23 @@ def post_create(request):
         'form':form
     }
     return render(request, 'blog/post_create.html',context)
+
+def post_detail(request,slug):
+    # obj = get_object_or_404(Post, slug=slug)
+    obj = Post.objects.get(slug=slug)
+    context = {
+        "object": obj
+    }
+    return render(request, "blog/post_detail.html", context)
+
+def post_update(request, slug):
+    obj = Post.objects.get(slug=slug)
+    form = PostForm(request.POST or None, request.FILES or None, instance=obj)
+    if form.is_valid():
+        form.save()
+        return redirect("blog:list")
+    context = {
+        "object": obj,
+        "form": form
+    }
+    return render(request, "blog/post_update.html", context)
